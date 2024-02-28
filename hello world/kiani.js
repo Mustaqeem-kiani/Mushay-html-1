7aba07150002d35de2be9e4a91eaf7c149b232b5
Mustaqeem-kiani <160250332+Mustaqeem-kiani@users.noreply.github.com>
2024-02-28 09:47:28 +0500

    Add files via upload

---
+++ hello world/kiani.js
@@ -0,0 +1,28 @@
+// // console.log("hello world");
+// // var namevar : string = "Mustaqeem";
+// // console.log(namevar);
+// let mus = 'kiani';
+// let ali = 16;
+// let mama = true;
+// // mama = english
+// mama = false;
+// // ali = 66;
+// mus = 'wawa';
+// const java = ( script) => {return script * ali;}
+// console.log (java (ali));
+// let personname = 'asim';
+// console.log ('wow '+ personname + ' zabardast yaar.') 
+// let myname: string= 'mustaqeEm';
+// console.log(myname . toLowerCase());
+// console.log(myname . toUpperCase());
+// let otherletter: string = myname.slice(0). toLowerCase ();
+// let firstletter: string = myname.charAt(1). toUpperCase ();
+// let titlecase:string = firstletter + otherletter;
+// console.log(titlecase);
+var amir = 'kiAni';
+console.log(amir.toLowerCase());
+console.log(amir.toUpperCase());
+var firstname = amir.charAt(0).toUpperCase();
+var bname = amir.slice(1).toLowerCase();
+var titlecase = firstname + bname;
+console.log(titlecase);
---
+++ hello world/kiani.ts
@@ -0,0 +1,50 @@
+// // console.log("hello world");
+
+// // var namevar : string = "Mustaqeem";
+// // console.log(namevar);
+
+// let mus = 'kiani';
+// let ali = 16;
+// let mama = true;
+
+// // mama = english
+// mama = false;
+
+// // ali = 66;
+
+// mus = 'wawa';
+
+// const java = ( script) => {return script * ali;}
+// console.log (java (ali));
+
+
+// let personname = 'asim';
+// console.log ('wow '+ personname + ' zabardast yaar.') 
+
+// let myname: string= 'mustaqeEm';
+
+// console.log(myname . toLowerCase());
+// console.log(myname . toUpperCase());
+
+
+// let otherletter: string = myname.slice(0). toLowerCase ();
+// let firstletter: string = myname.charAt(1). toUpperCase ();
+
+// let titlecase:string = firstletter + otherletter;
+
+// console.log(titlecase);
+
+
+
+const amir = 'kiAni';
+
+console.log(amir.toLowerCase());
+console.log(amir.toUpperCase());
+
+let firstname = amir.charAt(0).toUpperCase();
+let bname = amir.slice(1).toLowerCase();
+
+let titlecase = firstname + bname;
+
+console.log(titlecase);
+
